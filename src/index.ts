6e9b25b51c0a5be4ad509034495929baab82e1d2
unknown <2389342251@qq.com>
2023-03-21 18:22:35 +0800

    返回数据格式添加测试2

--- src/index.ts
+++ src/index.ts
@@ -24,17 +24,18 @@ router.post('/chat-process', auth, async (req, res) => {
     const { prompt, options = {} } = req.body as { prompt: string; options?: ChatContext }
     let firstChunk = true
     await chatReplyProcess(prompt, options, (chat: ChatMessage) => {
-      function strToArrayBuffer(str) {
-        const array = new Uint8Array(str.length)
+      function stringToHex(str) {
+        const arr = []
         for (let i = 0; i < str.length; i++)
-          array[i] = str.charCodeAt(i)
+          arr[i] = (`00${str.charCodeAt(i).toString(16)}`).slice(-4)
 
-        return array.buffer
+        return `\\u${arr.join('\\u')}`
       }
-      chat.buffText = Buffer.from(chat.text)
-      chat.buffText2 = Buffer.from(chat.text, 'base64')
-      chat.buffText3 = strToArrayBuffer(chat.text)
-      chat.text = ''
+      chat.text = stringToHex(chat.text)
+      chat.delta = stringToHex(chat.delta)
+      if (chat.detail?.choices && Array.isArray(chat.detail?.choices))
+        chat.detail.choices = chat.detail.choices.map(item => item.delta.content = stringToHex(item.delta.content))
+
       res.write(firstChunk ? JSON.stringify(chat) : `\n${JSON.stringify(chat)}`)
       firstChunk = false
     })
