e85b094b8a6f1a08c766e37fb6b26450741353f3
unknown <2389342251@qq.com>
2023-03-27 17:09:37 +0800

    body参数处理

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const app = express()
 const router = express.Router()
 
 // app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
+// app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.all('*', (_, res, next) => {
